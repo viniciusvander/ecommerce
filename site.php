e3765c9d7a1ddc60df7f889f938551055b50d87b
Vinicius Vander <viniciusvander1@gmail.com>
2020-10-09 19:28:51 -0300

    Carrinho Calculo de Frete

--- site.php
+++ site.php
@@ -58,6 +58,7 @@ $app->get("/products/:desurl", function($desurl){
 	$page->setTpl ("product-detail", [
 		'product'=>$product->getValues(),
 		'categories'=>$product->getCategories()
+
 	]);
 
 });
@@ -70,7 +71,8 @@ $app->get("/cart", function(){
 
 	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 
 });
@@ -123,6 +125,16 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	header("Location: /cart");
 	exit;
 
+});
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location: /cart");
+	exit;
 });
 
 
