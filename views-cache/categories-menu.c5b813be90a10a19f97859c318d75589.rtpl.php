43af0a46cb085088b99cd5faa2d68176c327e5dc
Vinicius Vander <viniciusvander1@gmail.com>
2020-10-09 15:17:59 -0300

    Carrinho Produtos

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/3">Android</a><li><a href="/categories/4">Apple</a><li><a href="/categories/5">Motorola</a><li><a href="/categories/6">Samsung</a>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/3">Android</a><li><a href="/categories/6">Apple</a><li><a href="/categories/4">Motorola</a><li><a href="/categories/2">Samsung</a>
